4af0e9398848c228eb1877c07893b5d76842539e
Sheplu <jean.burellier@gmail.com>
2016-03-12 17:11:41 +0100

    update

--- drinker-ionic/www/js/controllers.js
+++ drinker-ionic/www/js/controllers.js
@@ -40,34 +40,6 @@ angular.module('Drinker.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
-
-  // Create the reserve modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/reserve.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.reserveform = modal;
-  });
-
-  // Triggered in the reserve modal to close it
-  $scope.closeReserve = function() {
-    $scope.reserveform.hide();
-  };
-
-  // Open the reserve modal
-  $scope.reserve = function() {
-    $scope.reserveform.show();
-  };
-
-  // Perform the reserve action when the user submits the reserve form
-  $scope.doReserve = function() {
-    console.log('Doing reservation', $scope.reservation);
-
-    // Simulate a reservation delay. Remove this and replace with your reservation
-    // code if using a server system
-    $timeout(function() {
-      $scope.closeReserve();
-    }, 1000);
-  };
 })
 
 .controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory',
@@ -146,19 +118,10 @@ angular.module('Drinker.controllers', [])
     };
 }])
 
-.controller('IndexController', ['$scope', 'menuFactory', 'promotionFactory',
-'corporateFactory', 'promotion', 'leader', 'dish', 'baseURL', function ($scope,
-  menuFactory, promotionFactory, corporateFactory, promotion, leader,
-  dish, baseURL) {
-
+.controller('IndexController', ['$scope', 'baseURL', function ($scope, baseURL) {
     $scope.baseURL = baseURL;
-    $scope.leader = leader
 
-    $scope.showDish = false;
-    $scope.message = "Loading ...";
 
-    $scope.dish = dish;
-    $scope.promotion = promotion
 
 }])
 
--- drinker-ionic/www/js/services.js
+++ drinker-ionic/www/js/services.js
@@ -3,7 +3,6 @@
 angular.module('Drinker.services', ['ngResource'])
         .constant("baseURL","http://localhost:3000/")
         .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-
             return $resource(baseURL + "dishes/:id", null, {
                 'update': {
                     method: 'PUT'
