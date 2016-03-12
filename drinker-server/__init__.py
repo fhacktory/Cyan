1d91609c353063fa5154fda68c95d3d3e94f03cd
Cyril DEMINGEON <cyrbil@@users.noreply.github.com>
2016-03-12 17:07:47 +0100

    lbzelbl

---
+++ drinker-server/__init__.py
@@ -0,0 +1,69 @@
+import re
+import sys
+import logging
+import inspect
+from flask import Flask, request, jsonify
+from flask.blueprints import Blueprint
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+class Api(object):
+    def __init__(self, app):
+        self.app = app
+
+        # create routes
+        route_comment = re.compile(r'(GET|PUT|POST|DELETE)\s*:\s*([^\s]+)', re.I)
+        for method in inspect.getmembers(self, predicate=inspect.ismethod):
+            match = route_comment.match(str(method[1].__doc__).strip())
+            if match:
+                verb = match.group(1)
+                route = match.group(2)
+
+                self.app.add_url_rule(
+                    rule='/api/' + route,
+                    endpoint=method[0],
+                    view_func=self._endpoint_wrapper(method[1]),
+                    methods=[verb]
+                )
+                logger.debug('Route created: %6s - %s' % (method, route))
+
+    def _endpoint_wrapper(self, func):
+        def run(*args, **kwargs):
+            res = {'status': 'error', 'data': 'No data'}
+            try:
+                data = request.get_json(force=True, silent=True)
+                res_data = func(data, *args, **kwargs)
+
+                res = jsonify({
+                    'status': 'ok',
+                    'data': res_data
+                })
+            except Exception as err:
+                res = jsonify({
+                    'status': 'error',
+                    'data': str(err)
+                })
+            finally:
+                return res
+        return run
+
+    def hello_world(self, data):
+        """
+            GET: /
+        """
+        return 'Hello World'
+
+    def user_create(self, data):
+        """
+            POST: /user
+        """
+        if 'email' not in data:
+            raise Exception('No email send')
+
+app = Flask(__name__)
+api = Api(app)
+
+if __name__ == '__main__':
+    app.run()
---
+++ drinker-web/app/scripts/controllers.js
@@ -0,0 +1,137 @@
+'use strict';
+
+angular.module('DrinkerApp')
+
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+
+            $scope.showMenu = true;
+            $scope.message = "Loading ...";
+            $scope.dishes = menuFactory.getDishes().query();
+
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
+
+
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+.controller('ContactController', ['$scope', function($scope) {
+      $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+      var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+      $scope.channels = channels;
+      $scope.invalidChannelSelection = false;
+}])
+
+.controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
+    $scope.sendFeedback = function() {
+        if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        }
+        else {
+            $scope.invalidChannelSelection = false;
+            console.log($scope.feedback);
+            feedbackFactory.sendFeedback().save($scope.feedback);
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.feedback.mychannel="";
+            $scope.feedbackForm.$setPristine();
+        }
+    };
+}])
+
+.controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
+
+        $scope.leader = corporateFactory.getLeader().get({id:3});
+        $scope.showLeader = true;
+        $scope.messageLeader="Loading ...";
+        $scope.leader.$promise.then(
+          function(response) {
+            console.log(response);
+            $scope.leader = response;
+            $scope.showLeader = true;
+          },
+          function(response){
+            console.log(response);
+            $scope.messageLeader = "Error: "+response.status + " " + response.statusText;
+            $scope.showLeader = false;
+          }
+        )
+
+        $scope.promotion = menuFactory.getPromotion().get({id:0});
+        $scope.showPromo = true;
+        $scope.messagePromo="Loading ...";
+        $scope.promotion.$promise.then(
+          function(response) {
+            console.log(response);
+            $scope.promotion = response;
+            $scope.showPromo = true;
+          },
+          function(response){
+            console.log(response);
+            $scope.messagePromo = "Error: "+response.status + " " + response.statusText;
+            $scope.showPromo = false;
+          }
+        )
+
+
+        $scope.dish = {};
+        $scope.showDish = true;
+        $scope.message="Loading ...";
+        $scope.dish = menuFactory.getDishes().get({id:0});
+}])
+
+.controller('AboutController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
+        $scope.leaders = corporateFactory.getLeaders().query();
+        $scope.showLeaders = true;
+        $scope.messageLeaders="Loading ...";
+        $scope.leaders.$promise.then(
+          function(response) {
+            console.log(response);
+            $scope.leaders = response;
+            $scope.showLeaders = true;
+          },
+          function(response){
+            console.log(response);
+            $scope.messageLeaders = "Error: "+response.status + " " + response.statusText;
+            $scope.showLeaders = false;
+          }
+        )
+}])
+
+;
